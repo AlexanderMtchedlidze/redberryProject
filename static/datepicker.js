28334e507b34dadeec142da4817e13a56d746149
AlexanderMtchedlidze <mtchedlidzealexander@gmail.com>
2023-02-09 11:06:36 +0400

    this is fucking impossible

--- static/datepicker.js
+++ static/datepicker.js
@@ -1,33 +1,37 @@
 $(document).ready(function () {
-    $('#startDate').datepicker({
-        format: "dd/mm/yyyy",
-        autoclose: true
-    }).on('changeDate', function (selectedDate) {
-        var formattedDate = selectedDate.date.getDate() + "/" + (selectedDate.date.getMonth() + 1) + "/" + selectedDate.date.getFullYear();
-        localStorage.setItem("startDate", formattedDate);
-        localStorage.setItem("startDateValidation", true);
-        document.getElementById("startTime").innerHTML = formattedDate;
-        document.getElementById("startTime").style.display = "unset"
-        document.getElementById("experienceTitle").style.display = "unset";
-        $(this).css("outline", "0.5px solid #98E37E");
-        $(this).css("border", "0.5px solid #98E37E");
-        checkInputs();
-    });
+    document.querySelectorAll(".startDate").forEach(element => {
+        $(element).datepicker({
+            format: "dd/mm/yyyy",
+            autoclose: true
+        }).on('changeDate', function (selectedDate) {
+            var formattedDate = selectedDate.date.getDate() + "/" + (selectedDate.date.getMonth() + 1) + "/" + selectedDate.date.getFullYear();
+            localStorage.setItem("startDate", formattedDate);
+            localStorage.setItem("startDateValidation", true);
+            document.getElementById("startTime").innerHTML = formattedDate;
+            document.getElementById("startTime").style.display = "unset"
+            document.getElementById("experienceTitle").style.display = "unset";
+            $(this).css("outline", "0.5px solid #98E37E");
+            $(this).css("border", "0.5px solid #98E37E");
+            checkInputs();
+        });
+    })
 
-    $('#endDate').datepicker({
-        format: "dd/mm/yyyy",
-        autoclose: true,
-    }).on('changeDate', function (selectedDate) {
-        var formattedDate = selectedDate.date.getDate() + "/" + (selectedDate.date.getMonth() + 1) + "/" + selectedDate.date.getFullYear();
-        document.getElementById("endTime").innerHTML = formattedDate;
-        localStorage.setItem("endDate", formattedDate);
-        document.getElementById("experienceTitle").style.display = "unset";
-        document.getElementById("endTime").style.display = "unset"
-        localStorage.setItem("endDateValidation", true);
-        $(this).css("outline", "0.5px solid #98E37E");
-        $(this).css("border", "0.5px solid #98E37E");
-        checkInputs();
-    });
+    document.querySelectorAll(".endDate").forEach(element => {
+        $(element).datepicker({
+            format: "dd/mm/yyyy",
+            autoclose: true,
+        }).on('changeDate', function (selectedDate) {
+            var formattedDate = selectedDate.date.getDate() + "/" + (selectedDate.date.getMonth() + 1) + "/" + selectedDate.date.getFullYear();
+            document.getElementById("endTime").innerHTML = formattedDate;
+            localStorage.setItem("endDate", formattedDate);
+            document.getElementById("experienceTitle").style.display = "unset";
+            document.getElementById("endTime").style.display = "unset"
+            localStorage.setItem("endDateValidation", true);
+            $(this).css("outline", "0.5px solid #98E37E");
+            $(this).css("border", "0.5px solid #98E37E");
+            checkInputs();
+        });
+    })
 });
 
 function checkInputs() {
